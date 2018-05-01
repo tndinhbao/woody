5b2ca8f29b8b1c3c8d656e36340a66402dd46ceb
Bao Tran <tndinhbao@gmail.com>
2018-05-01 11:49:36 +0700

    w-3: add material-ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { withStyles } from 'material-ui/styles'
+import Appbar from './components/Appbar'
+import TopBanner from './components/TopBanner'
+import TopCategory from './components/TopCategory'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = ({ classes }) => {
+  return (
+    <div className={classes.root}>
+      <Appbar />
+      <div className={classes.banner}>
+        <TopBanner />
       </div>
-    );
-  }
+      <div>
+        <TopCategory />
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default withStyles(theme => ({
+  banner: {
+    marginTop: theme.spacing.unit
+  }
+}))(App)
---
+++ src/components/UI/Grid.js
@@ -0,0 +1,2 @@
+export { default } from 'material-ui/Grid'
+export * from 'material-ui/Grid'
